eb6e5db2d1a7ebcfb7aa7328e47f3ca5c9db790e
CrisNAC <cristian.acuna@fiuni.edu.py>
2025-04-19 20:58:50 -0300

    Huesped controller y route listo

--- backend/src/controllers/ingreso.controller.js
+++ backend/src/controllers/ingreso.controller.js
@@ -1,12 +1,11 @@
 import { PrismaClient } from '../../generated/prisma';
 const prisma = new PrismaClient();
 
-
 const getAllIngresos = async (req, res) => {
     try {
-        const ingresos = await prisma.ingreso.findMany();
-        if (!ingresos) return res.status(404).json({ error: 'Tareas no enontradas' });
-        res.json(ingresos);
+        const result = await prisma.ingreso.findMany();
+        if (!result) return res.status(404).json({ error: 'Tareas no enontradas' });
+        res.json(result);
     } catch (error) {
         res.status(500).json({ error: 'Error al obtener tareas' });
     }
