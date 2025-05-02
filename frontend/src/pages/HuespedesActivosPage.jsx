364182121ba40b968ed98fc97f6e0226f9ff06da
CrisNAC <cristian.acuna@fiuni.edu.py>
2025-05-01 21:49:24 -0300

    Agregacion de skeleton en HuespedesActivosPage y controladores en Ingresos

--- frontend/src/pages/HuespedesActivosPage.jsx
+++ frontend/src/pages/HuespedesActivosPage.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react';
-//import { useNavigate } from 'react-router-dom';
 import HTTPClient from '../api/HTTPClient';
 import NavBar from '../components/navbar';
 import HuespedesActivos from '../components/HuespedesActivos.jsx';
 import NavBarSkeleton from '../skeleton/narbar.skeleton.jsx';
+import { Skeleton } from '@mui/material';
+import { Container } from 'react-bootstrap';
 
 function HuespedesActivosPage() {
     const client = new HTTPClient();
     const [ingresosOriginales, setIngresosOriginales] = useState([]);
+    const [mainPage, setMainPage] = useState(true);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    //const navigate = useNavigate();
 
     useEffect(() => {
         const fetchIngresos = async () => {
@@ -27,28 +28,51 @@ function HuespedesActivosPage() {
         fetchIngresos();
     }, []);
 
-    const skeletonPage = () => {
-        return (
-            <>
-                <NavBarSkeleton></NavBarSkeleton>
-                <div className="text-center py-5">Cargando...</div>
-            </>
-        );
-    };
+    const skeletonPage = () => (
+        <>
+            <NavBarSkeleton />
+            <Container>
+                <div className='d-flex justify-content-center' style={{ marginTop: "50px" }}><Skeleton variant='text' animation='wave' width={180} height={60}></Skeleton></div>
+                <div className='d-flex justify-content-center mb-3'><Skeleton variant='rectangular' animation='wave' width={1200} height={100}></Skeleton></div>
+                <div className='d-flex justify-content-center'><Skeleton variant='rectangular' animation='wave' width={1200} height={350}></Skeleton></div>
+                <div className='d-flex justify-content-center mt-2'>
+                    <div className='d-flex'>
+                        <Skeleton variant='rectangular' animation='wave' width={100} height={30} style={{ margin: "0 0.2rem 0 0.2rem" }}></Skeleton>
+                        <Skeleton variant='rectangular' animation='wave' width={30} height={30} style={{ margin: "0 0.2rem 0 0.2rem" }}></Skeleton>
+                        <Skeleton variant='rectangular' animation='wave' width={30} height={30} style={{ margin: "0 0.2rem 0 0.2rem" }}></Skeleton>
+                        <Skeleton variant='rectangular' animation='wave' width={30} height={30} style={{ margin: "0 0.2rem 0 0.2rem" }}></Skeleton>
+                        <Skeleton variant='rectangular' animation='wave' width={100} height={30} style={{ margin: "0 0.2rem 0 0.2rem" }}></Skeleton>
+                    </div>
+                </div>
+            </Container>
+        </>
+    );
 
-    const errorPage = () => {
-        return (<div className="text-center py-5 text-danger">Error: {error}</div>);
-    };
+    const errorPage = () => (
+        <div className="text-center py-5 text-danger">Error: {error}</div>
+    );
 
-    if (loading) skeletonPage();
-    if (error) errorPage();
+    if (loading) return skeletonPage();
+    if (error) return errorPage();
 
     return (
         <>
-            <NavBar />
-            <div className="p-6 max-w-7xl mx-auto">
-                <HuespedesActivos ingresosOriginales={ingresosOriginales} />
-            </div>
+            {
+                loading ? skeletonPage() :
+                    error ? errorPage() :
+                        (
+                            <>
+                                <NavBar />
+                                {mainPage ?
+                                    <Container>
+                                        <HuespedesActivos ingresosOriginales={ingresosOriginales} />
+                                    </Container>
+                                    :
+                                    <h1>Pantalla de consumos</h1>
+                                }
+                            </>
+                        )
+            }
         </>
     );
 }
