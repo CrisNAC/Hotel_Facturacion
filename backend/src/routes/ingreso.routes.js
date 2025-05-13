6b9eb47721b466baa4bc0dad3d40e0d4959ddd3a
seba_kisser <skf.sokc@gmail.com>
2025-05-13 13:37:04 -0300

    Actualizacion de rutas

--- backend/src/routes/ingreso.routes.js
+++ backend/src/routes/ingreso.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { getAllIngresos, createIngreso, cancelarIngreso } from '../controllers/ingreso.controllers.js';
-
+import authenticate from "../../config/jwt.config.js"
 const router = Router();
 
 router.get('/', getAllIngresos);
