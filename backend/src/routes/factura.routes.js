a6b8a1b777152307ae39cd208453075b0fcf22b3
CrisNAC <cristian.acuna@fiuni.edu.py>
2025-05-27 19:12:45 -0300

    Arreglando falta de importacion de funcion en factura.routes

--- backend/src/routes/factura.routes.js
+++ backend/src/routes/factura.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { getAllFacturas, createFactura, getFacturaById, obtenerUltimoNumeroFactura, enviarFacturaDesdeArchivo } from "../controllers/factura.controllers.js";
+import { getAllFacturas, createFactura, getFacturaById, obtenerUltimoNumeroFactura, enviarFacturaDesdeArchivo, getFacturasPorFechas } from "../controllers/factura.controllers.js";
 import upload from '../services/multer.js';
-import {  } from '../controllers/factura.controllers.js';
+import { } from '../controllers/factura.controllers.js';
 
 const router = Router();
 
-router.get("/ultimafactura",obtenerUltimoNumeroFactura)
+router.get("/ultimafactura", obtenerUltimoNumeroFactura)
 router.get("/", getAllFacturas);
 router.post("/", createFactura);
 router.get("/fechas", getFacturasPorFechas);
