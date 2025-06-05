cd06ec1aaf4b824769461418b68e76d2d920918a
J-Kanami-PS <jazminpavon@fiuni.edu.py>
2025-06-04 21:02:58 -0300

    Eliminando duplicado Jazmín 04/06

--- backend/src/routes/huespedhabitacion.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAllHuespedHabitacion } from '../controllers/huespedhabitacion.controllers.js';
-import authenticate from "../../config/jwt.config.js";
-
-const router = Router();
-
-router.get('/:id', authenticate, getAllHuespedHabitacion);
-
-export default router;
